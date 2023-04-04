0dec9c627feb3d016b8f9fbdf0bf8a84a38d43b9
Thanoraj Muthulingam <90021137+Thanoraj@users.noreply.github.com>
2023-04-04 22:10:33 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# Define a text input field
+text_input = st.text_input('Enter your name')
+
+# Define a slider
+slider_val = st.slider('Select a value', 1, 5, 1)
+
+# Define a submit button
+submit_button = st.button('Submit')
+
+# Handle button click event
+if submit_button:
+    st.write('Hello, {}! You selected {} on the slider.'.format(text_input, slider_val))
